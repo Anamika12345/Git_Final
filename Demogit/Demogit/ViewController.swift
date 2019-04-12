a90430ca59a87f114f1549a2b89fcec88073d075
anamika.sinha@mashvirtual.in <anamika.sinha@mashvirtual.in>
2019-04-12 16:16:28 +0530

    Initial Commit

---
+++ Demogit/Demogit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demogit
+//
+//  Created by Mash Virtual on 12/04/19.
+//  Copyright © 2019 Mash Virtual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
